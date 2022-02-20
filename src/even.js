91df2a3429e781b79366c79d0aba53f2d286a09b
Ana <anastasiapak32@gmail.com>
2022-02-20 18:45:45 +0300

    brain-even

---
+++ src/even.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+  };
+
+const isEven = (num) => {
+    return (num % 2 === 0) ? 'yes' : 'no';
+};
+
+export default () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    let counter = 0;
+    for (let questionsCount = 1; questionsCount <= 3; questionsCount += 1) {
+    const number = getRandomNumber(100);
+    const correctAnswer = isEven(number);
+
+      console.log(`Question: ${number}`);
+      const answer = readlineSync.question('Your answer: ');
+      if (answer === correctAnswer) {
+          console.log('Correct!');
+          counter += 1;
+          
+      } else {
+          console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+          break;
+      }
+    };
+
+    const finalMessage = (counter === 3) ? `Congratulations, ${name}!` : `Let's try again, ${name}!`;
+
+    console.log(finalMessage);
+};
