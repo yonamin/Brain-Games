b97a932b06215c10ccfa937eb3ab3371baf03b2a
Ana <anastasiapak32@gmail.com>
2022-02-24 22:54:52 +0300

    edit by linter

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,41 +1,40 @@
 import {
-    greeting, getRandomNum, level, finalMessage,
-  } from '../index.js';
+  greeting, getRandomNum, level, finalMessage,
+} from '../index.js';
 
-  let spacedNum = 0;
-  const progression = (firstNum, step, space) => {
-      const prog = [firstNum];
-      let currentNum = firstNum;
-      for (let i = 0; i < 10; i += 1) {
-          currentNum += step;
-          if (i !== space) {
-              prog.push(currentNum);
-          } else {
-              prog.push('..')
-              spacedNum = currentNum;
-          }
-      }
-    return prog;
-  };
+let spacedNum = 0;
+const progression = (firstNum, step, space) => {
+  const prog = [firstNum];
+  let currentNum = firstNum;
+  for (let i = 0; i < 10; i += 1) {
+    currentNum += step;
+    if (i !== space) {
+      prog.push(currentNum);
+    } else {
+      prog.push('..');
+      spacedNum = currentNum;
+    }
+  }
+  return prog;
+};
 
-  export default () => {
-      greeting();
-      console.log('What number is missing in the progression?');
+export default () => {
+  greeting();
+  console.log('What number is missing in the progression?');
 
-      let result;
-      let correctCounter = 0;
-      do {
-          const step = getRandomNum(1, 5);
-          const firstNum = getRandomNum(1, 100);
-          const space = getRandomNum(0, 9);
-          const question = progression(firstNum, step, space).join(' ');
-          const correctAnswer = String(spacedNum);
-          result = level(question, correctAnswer);
-          if (result === 'true') {
-          correctCounter += 1;
-       }
-    } while (result === 'true' && correctCounter < 3);
-  
-    finalMessage(correctCounter);
-      
-  };
+  let result;
+  let correctCounter = 0;
+  do {
+    const step = getRandomNum(1, 5);
+    const firstNum = getRandomNum(1, 100);
+    const space = getRandomNum(0, 9);
+    const question = progression(firstNum, step, space).join(' ');
+    const correctAnswer = String(spacedNum);
+    result = level(question, correctAnswer);
+    if (result === 'true') {
+      correctCounter += 1;
+    }
+  } while (result === 'true' && correctCounter < 3);
+
+  finalMessage(correctCounter);
+};
